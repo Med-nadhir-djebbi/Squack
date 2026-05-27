ab663b76d8851e6dbd1e7409475fd1c0327a23a3
Mohamed Nadhir Djebbi <mohamednadhirdjebbi@gmail.com>
2026-05-27 21:35:14 +0100

    nothing

--- apps/backend/src/prisma/prisma.module.ts
+++ apps/backend/src/prisma/prisma.module.ts
@@ -5,7 +5,7 @@ import { PrismaService } from './prisma.service';
 @Global()
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService],
+  exports: [PrismaService],   
 =======
 import { Global, Module, } from '@nestjs/common';
 import { PrismaService } from './prisma.service';

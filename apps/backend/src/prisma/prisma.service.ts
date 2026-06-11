ab2ea800c0e6f46873750ff9b4434cafe07288cc
Mohamed Nadhir Djebbi <mohamednadhirdjebbi@gmail.com>
2026-06-11 15:45:18 +0100

    fixed the github workflow (properly)

--- apps/backend/src/prisma/prisma.service.ts
+++ apps/backend/src/prisma/prisma.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import {PrismaPg} from '@prisma/adapter-pg'
+import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor () {
-    super({ adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }) })
+  constructor() {
+    super({
+      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+    });
   }
   async onModuleInit(): Promise<void> {
     await this.$connect();
